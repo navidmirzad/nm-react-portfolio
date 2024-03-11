119b074776c7fa6f25b2dd38f830d0fdad698951
navidmirzad <navi0221@stud.kea.dk>
2024-03-11 12:33:44 +0100

    about almost done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,9 +3,16 @@ import nav from '../assets/nav.jpg'
 
 const About = () => {
   return (
-    <div className='w-full bg-[#ff8045 py-16 px-4]'>
+    <div className='w-full bg-[#ffffff] py-16 px-4'>
         <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
             <img src={nav} alt="/" />
+            <div>
+              <p>SOFTWARE, WEB, FULLSTACK DEV</p>
+              <h1>About me, Navid Mirzad</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Cumque distinctio repellat laboriosam omnis porro dolor! Nam placeat nulla sapiente.
+                Odit numquam libero pariatur in, voluptas architecto a necessitatibus perspiciatis magni?</p>
+            </div>
         </div>
     </div>
   )
